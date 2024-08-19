7d64b960de4987972031f24986cd2d6de1a71ae1
Shafihu Mustapha <shafihumustapha0101@gmail.com>
2024-08-19 13:04:11 +0000

    finished ui

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import styles from "@/app/ui/login/login.module.css";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className={styles.container}>
+      <form action="" className={styles.form}>
+        <h1>Login</h1>
+        <input type="text" placeholder="username" name="username" />
+        <input type="password" placeholder="password" name="password" />
+        <button>Login</button>
+      </form>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -1,7 +1,115 @@
 import React from "react";
+import styles from "../sidebar/sidebar.module.css";
+import {
+  MdDashboard,
+  MdSupervisedUserCircle,
+  MdShoppingBag,
+  MdAttachMoney,
+  MdWork,
+  MdAnalytics,
+  MdPeople,
+  MdOutlineSettings,
+  MdHelpCenter,
+  MdLogout,
+} from "react-icons/md";
+import MenuLink from "./menuLink/menuLink";
+import Image from "next/image";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const menuItems = [
+    {
+      title: "Pages",
+      list: [
+        {
+          title: "Dashboard",
+          path: "/dashboard",
+          icon: <MdDashboard />,
+        },
+        {
+          title: "Users",
+          path: "/dashboard/users",
+          icon: <MdSupervisedUserCircle />,
+        },
+        {
+          title: "Products",
+          path: "/dashboard/products",
+          icon: <MdShoppingBag />,
+        },
+        {
+          title: "Transactions",
+          path: "/dashboard/transactions",
+          icon: <MdAttachMoney />,
+        },
+      ],
+    },
+    {
+      title: "Analytics",
+      list: [
+        {
+          title: "Revenue",
+          path: "/dashboard/revenue",
+          icon: <MdWork />,
+        },
+        {
+          title: "Reports",
+          path: "/dashboard/reports",
+          icon: <MdAnalytics />,
+        },
+        {
+          title: "Teams",
+          path: "/dashboard/teams",
+          icon: <MdPeople />,
+        },
+      ],
+    },
+    {
+      title: "User",
+      list: [
+        {
+          title: "Settings",
+          path: "/dashboard/settings",
+          icon: <MdOutlineSettings />,
+        },
+        {
+          title: "Help",
+          path: "/dashboard/help",
+          icon: <MdHelpCenter />,
+        },
+      ],
+    },
+  ];
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.user}>
+        <Image
+          src="/noavatar.png"
+          alt="avatar"
+          width={50}
+          height={50}
+          className={styles.userImage}
+        />
+        <div className={styles.userDetails}>
+          <span className={styles.username}>John Mensah</span>
+          <span className={styles.userTitle}>Administrator</span>
+        </div>
+      </div>
+      <ul className={styles.list}>
+        {menuItems.map((cat) => (
+          <li key={cat.title}>
+            <span className={styles.cat}>{cat.title}</span>
+            {cat.list.map((item) => (
+              <MenuLink key={item.title} item={item} />
+            ))}
+          </li>
+        ))}
+      </ul>
+      <button className={styles.logout}>
+        <MdLogout />
+        Logout
+      </button>
+    </div>
+  );
 };
 
 export default Sidebar;
